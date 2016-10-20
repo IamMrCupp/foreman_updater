75d4c177ba8b5a7fcd3d6723f65325a1e51e0759
Aaron Cupp <mrcupp@mrcupp.com>
2016-10-19 19:51:11 -0700

    adding backup script

---
+++ foreman_backup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+##################################################################################################
+#                                                                                                #
+#   backup_foreman.sh                                                                            #
+#                                                                                                #
+#   Simple script for backing up the foreman setup                                               #
+#     Aaron Cupp  <mrcupp@mrcupp.com>                                                            #
+#                                                                                                #
+#   Purpose:  perform the steps needed to backup the foreman setup before an upgrade.            #
+#               These are outlined at https://theforeman.org/manuals/1.13/index.html#3.6Upgrade  #
+#                                                                                                #
+#   When:     2016-10-19                                                                         #
+#   Version:  1.1                                                                                #
+#                                                                                                #
+##################################################################################################
+
+# variables are nice
+#   -   Note:  ${HOME} is an environmental variable from the user who invoked the script
+#
+DATESTAMP=`date +%s`  # get this in epoch time
+BACKUP_DIR=${HOME}/backup-${DATESTAMP}
+
+# make a backup location and move to it
+mkdir -p ${BACKUP_DIR}
+cd ${BACKUP_DIR}
+
+# dump the foreman DB to a flatfile
+foreman-rake db:dump
+
+# move the foreman db dumpfile to the backup directory
+find /usr/share/foreman/ -name "foreman.*.sql" -exec mv {} ${BACKUP_DIR} \;
+find /var/lib/foreman/db/ -name "foreman.*.sql" -exec mv {} ${BACKUP_DIR} \;
+
+# backup the various firectories that foreman uses
+tar --selinux -czf etc_foreman_fire.tgz /etc/foreman
+tar --selinux -czf var_lib_puppet_dir.tgz /var/lib/puppet/ssl
+
+# cleanly exit things
+exit 0
