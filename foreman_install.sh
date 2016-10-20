b74c722ffd9bb7d9da5ec349d71e89763ea87a9e
Aaron Cupp <mrcupp@mrcupp.com>
2016-10-20 12:31:56 -0700

    framework

---
+++ foreman_install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+##################################################################################################
+#                                                                                                #
+#   What:     foreman_install.sh                                                                 #
+#                                                                                                #
+#   Who:      Aaron Cupp  <mrcupp@mrcupp.com>                                                    #
+#                                                                                                #
+#   Purpose:  Perform the steps needed to install foreman on a new server.                       #
+#             - Outlined at https://theforeman.org/manuals/1.11/index.html#2.1Installation       #
+#                                                                                                #
+#   When:     2016-10-20                                                                         #
+#   Version:  1.2                                                                                #
+#                                                                                                #
+##################################################################################################
+
